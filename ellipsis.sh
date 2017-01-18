5e7801c2adb590db42607f5b2fa60cd8020f3c2f
Peter Brunner <pbrunner@gmail.com>
2017-01-18 02:51:39 -0500

    init submodule on install

--- ellipsis.sh
+++ ellipsis.sh
@@ -3,17 +3,19 @@
 # lugoues/tmux ellipsis package
 
 # The following hooks can be defined to customize behavior of your package:
-# pkg.install() {
-#     fs.link_files $PKG_PATH
-# }
+ pkg.install() {
+     fs.link_files $PKG_PATH
+     git submodule update --init --recursive
+ }
 
 # pkg.push() {
 #     git.push
 # }
 
-# pkg.pull() {
-#     git.pull
-# }
+ pkg.pull() {
+     git.pull
+     git submodule update --recursive
+ }
 
 # pkg.installed() {
 #     git.status
